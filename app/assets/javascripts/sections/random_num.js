7139cd9abdeeb712b8c4ddec27263ec38235665b
Jake McHargue <web@jakemchargue.com>
2013-05-30 15:31:36 -0600

    break down long streams of numbers into decimal pieces

--- app/assets/javascripts/sections/random_num.js
+++ app/assets/javascripts/sections/random_num.js
@@ -1,9 +1,16 @@
 $(function () {
   $('input#number_number').keyup(function () {
     var decimals = $('.decimals input[type=radio]:checked').val()
-    if ($(this).val().length == decimals) {
-      $('form.number').submit();
-      $(this).val('');
+    if ($(this).val().length >= decimals) {
+      var nums = $(this).val().toString();
+      n = Math.ceil(nums.length/decimals);
+      for(i = 0; i < n; i++) {
+        var number = nums.substring(0, decimals);
+        var nums   = nums.substring(decimals);
+        $(this).val(number);
+        $('form.number').submit();
+        $(this).val(nums);
+      }
     }
   });
 });
