e861ef9d924e2d139c13d64b7a4e8d03afea1738
Jake McHargue <web@jakemchargue.com>
2013-05-29 22:47:57 -0600

    add random controller and simple view

---
+++ app/controllers/random_controller.rb
@@ -0,0 +1,4 @@
+class RandomController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 GenerationRandom::Application.routes.draw do
+
+  root :to => "random#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
