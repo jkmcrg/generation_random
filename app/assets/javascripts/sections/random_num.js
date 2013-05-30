ee452d90570e169cce528e7986e4e5c6346c5496
Jake McHargue <web@jakemchargue.com>
2013-05-30 15:37:36 -0600

    don't submit short numbers without leading zeroes

--- app/assets/javascripts/sections/random_num.js
+++ app/assets/javascripts/sections/random_num.js
@@ -3,7 +3,7 @@ $(function () {
     var decimals = $('.decimals input[type=radio]:checked').val()
     if ($(this).val().length >= decimals) {
       var nums = $(this).val().toString();
-      n = Math.ceil(nums.length/decimals);
+      n = Math.floor(nums.length/decimals);
       for(i = 0; i < n; i++) {
         var number = nums.substring(0, decimals);
         var nums   = nums.substring(decimals);
